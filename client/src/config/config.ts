65f84575c487a062bacc0f59c00c7343e2d294ab
Niko Tiikkaja <niko.tiikkaja@gmail.com>
2021-10-18 12:39:40 +0300

    Axios interceptor for 401 responses

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,5 +1,20 @@
+import axios from 'axios';
+import router from '../router/router';
+
 const config = {
     SERVICE_URL: 'http://localhost:5000'
 };
+
+axios.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        if (error.response.status === 401) {
+            router.push('/login');
+        }
+        return error;
+    }
+);
 
 export default config;
