09cdf1d61c5c829f7e9b1bfd4793c03e873d19eb
Niko Tiikkaja <niko.tiikkaja@gmail.com>
2021-10-18 14:21:08 +0300

    Move db creation from shell script to sql file

--- server/db/create_db.sql
+++ server/db/create_db.sql
@@ -1,3 +1,10 @@
+CREATE USER default_user WITH PASSWORD 'pass';
+CREATE DATABASE todo;
+GRANT ALL PRIVILEGES ON DATABASE todo TO default_user;
+\connect todo default_user
+BEGIN;
+COMMIT;
+
 CREATE TABLE IF NOT EXISTS account (
     id INT NOT NULL PRIMARY KEY,
     name VARCHAR(128) NOT NULL,
